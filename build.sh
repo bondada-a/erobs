b1c2b8225725bbf155881378a21dbf7c04b25964
bondada-a <bondada.a@northeastern.edu>
2025-06-23 13:21:11 -0400

    skip zed

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ source /opt/ros/${ROS_DISTRO}/setup.bash && colcon build \
 colcon build \
         --merge-install \
         --symlink-install \
-        --packages-skip zed-ros2-wrapper\
+        --packages-skip zed_components\
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
