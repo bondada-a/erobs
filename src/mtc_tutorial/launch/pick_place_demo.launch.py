0f3561eb05de0c3c0c9f78731984d5f6ca260e41
bondada-a <bondada.a@northeastern.edu>
2025-07-03 12:47:33 -0400

    Updated dependencies

---
+++ src/mtc_tutorial/launch/pick_place_demo.launch.py
@@ -0,0 +1,31 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from ament_index_python.packages import get_package_share_directory
+import os
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("ur5e_hande")
+        .robot_description(file_path="config/ur.urdf.xacro")
+        .to_moveit_configs()
+    )
+
+    # MTC Demo node
+    pick_place_demo = Node(
+        package="mtc_tutorial",
+        executable="mtc_tutorial",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+        ],
+    )
+
+    return LaunchDescription([pick_place_demo])
