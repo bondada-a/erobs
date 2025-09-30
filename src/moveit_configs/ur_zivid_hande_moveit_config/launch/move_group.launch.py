a6162ad18aa9182937e359e11b6022cbd091c3de
bondada-a <bondada.a@northeastern.edu>
2025-09-30 11:35:37 -0400

    remove unused header and parameter

--- src/moveit_configs/ur_zivid_hande_moveit_config/launch/move_group.launch.py
+++ src/moveit_configs/ur_zivid_hande_moveit_config/launch/move_group.launch.py
@@ -4,7 +4,6 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch import LaunchDescription
-from launch.conditions import IfCondition
 from ament_index_python.packages import get_package_share_directory
 import os
 from launch.actions import IncludeLaunchDescription
@@ -16,7 +15,6 @@ def generate_launch_description():
 
     ur_type = DeclareLaunchArgument('ur_type', default_value='ur5e')
     robot_ip = DeclareLaunchArgument('robot_ip', default_value='192.168.1.10')
-    use_fake_hardware = DeclareLaunchArgument('use_fake_hardware', default_value='false')
     description_package = DeclareLaunchArgument('description_package', default_value='ur_description')
     description_file = DeclareLaunchArgument('description_file', default_value=os.path.join(get_package_share_directory("ur5e_hande_robot_description"), "urdf", "ur_with_zivid_hande.xacro"))
     controllers_file = DeclareLaunchArgument('controllers_file', default_value=os.path.join(get_package_share_directory("ur_zivid_hande_moveit_config"), "config", "ur_hande_controllers.yaml"))
@@ -133,7 +131,6 @@ def generate_launch_description():
         ## arguments 
         robot_ip,
         ur_type,
-        use_fake_hardware,
         description_package,
         description_file,
         controllers_file,
