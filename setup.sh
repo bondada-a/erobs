efef735a97c36a85275bedd98df56ba08c881069
bondada-a <bondada.a@northeastern.edu>
2025-10-29 15:15:35 -0400

    vision to bsui

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -e
 
 vcs import < src/ros2.repos src
 vcs import < src/end_effectors/end_effectors.repos src/end_effectors
+vcs import < src/vision/vision.repos src/vision
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y --skip-keys 'moveit_studio_behavior_interface zivid_description'
